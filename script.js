4b496acd22cbb5cf2f75ee9bf2f32eecd79cdb70
MartinHoffNygaard <martinhn@outlook.com>
2023-03-15 15:05:45 +0100

    Changes to script.js -Created variables from classes/ids from html. -Created start conditions for a new game.
    Changes to style.css:
    -Create a hidden class to change display of elements.
    
    Changes to index.html:
    -Corrected the path for dice image.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+'use strict';
 
+// Creating variables from classes and id in the html document.
+const elementScore0 = document.querySelector('#score--0'); // The scoreboard for player0 score.
+const elementScore1 = document.querySelector('#score--1'); // The scoreboard for player1 score.
+const elementDice = document.querySelector('.dice'); //The dice.
+
+// Setting the start conditions for a new game.
+elementScore0.textContent = 0;
+elementScore1.textContent = 0;
+elementDice.classList.add('hidden');

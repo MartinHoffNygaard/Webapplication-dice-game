710fd51ebd2aad121edebf93d934423e4a77285c
MartinHoffNygaard <martinhn@outlook.com>
2023-03-15 16:36:52 +0100

    Changes to script.js: -Added gamestate variable. -Created switchPlayer function. -Added gamestate check to button clicks. -Added functionality to the hold button

--- script.js
+++ script.js
@@ -15,34 +15,66 @@ const btnHold = document.querySelector('.btn--hold'); // Button to hold the curr
 const scores = [0, 0]; // Array to hold scores for players.
 let currentScore = 0; // Variable to hold the current score.
 let currentPlayer = 0; //Variable to hold the current player.
+let playing = true; // Variable to store the gamestate.
 
 // Setting the start conditions for a new game.
 elementScore0.textContent = 0;
 elementScore1.textContent = 0;
 elementDice.classList.add('hidden');
 
+//Function to switch player.
+const switchPlayer = function () {
+  document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  elementPlayer0.classList.toggle('player--active'); // Changes the display of the element.
+  elementPlayer1.classList.toggle('player--active'); // Changes the display of the element.
+};
+
 // Function to roll the dice when roll button is pressed.
 btnRoll.addEventListener('click', function () {
-  // Creates a random number between 1-6.
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // Creates a random number between 1-6.
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // Shows the correct dice image equal to the rolled dice.
-  elementDice.classList.remove('hidden');
-  elementDice.src = `images/dice-${dice}.png`;
+    // Shows the correct dice image equal to the rolled dice.
+    elementDice.classList.remove('hidden');
+    elementDice.src = `images/dice-${dice}.png`;
 
-  // If dice is not equal to 1 it adds the dice number to current score and displays it.
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
+    // If dice is not equal to 1 it adds the dice number to current score and displays it.
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+    }
+
+    // If dice is 1 it will call the switch player function.
+    else {
+      switchPlayer();
+    }
   }
+});
 
-  // If dice is 1 it will reset the current score to 0 and switch the current player.
-  else {
-    document.getElementById(`current--${currentPlayer}`).textContent = 0;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    elementPlayer0.classList.toggle('player--active'); // Changes the display of the element.
-    elementPlayer1.classList.toggle('player--active'); // Changes the display of the element.
+// Functionality when pressing the hold button.
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Adds the current socre to the total score of the player and displays it.
+    scores[currentPlayer] += currentScore;
+    document.getElementById(`score--${currentPlayer}`).textContent =
+      scores[currentPlayer];
+
+    // Check if the currentplayer has won the game. Changes display if true. Switches player if false.
+    if (scores[currentPlayer] >= 100) {
+      playing = false;
+      elementDice.classList.add('hidden');
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.remove('player-active');
+    } else {
+      switchPlayer();
+    }
   }
 });

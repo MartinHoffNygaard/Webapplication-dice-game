7e5a0f1dd644e047ca6e6c6d21246915fc9e1ca0
MartinHoffNygaard <martinhn@outlook.com>
2023-03-15 15:54:18 +0100

    Changes to script.js: -Added variables for the display around the player -Added array to hold the scores -Added functionality to change player and reset current score when dice is 1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Creating variables from classes and id in the html document.
+const elementPlayer0 = document.querySelector('.player--0'); // The element for player 0.
+const elementPlayer1 = document.querySelector('.player--1'); // The element for player 1.
 const elementScore0 = document.querySelector('#score--0'); // The scoreboard for player0 score.
 const elementScore1 = document.querySelector('#score--1'); // The scoreboard for player1 score.
 const elementCurrentScore0 = document.getElementById('current--0'); // Current score for player0.
@@ -10,7 +12,9 @@ const btnNew = document.querySelector('.btn--new'); // Button to start new game.
 const btnRoll = document.querySelector('.btn--roll'); // Button to roll the dice.
 const btnHold = document.querySelector('.btn--hold'); // Button to hold the current score.
 
-let currentScore = 0; // Variable to hold the current score
+const scores = [0, 0]; // Array to hold scores for players.
+let currentScore = 0; // Variable to hold the current score.
+let currentPlayer = 0; //Variable to hold the current player.
 
 // Setting the start conditions for a new game.
 elementScore0.textContent = 0;
@@ -26,10 +30,19 @@ btnRoll.addEventListener('click', function () {
   elementDice.classList.remove('hidden');
   elementDice.src = `images/dice-${dice}.png`;
 
-  // If dies is not equal to 1 it adds the dice number to current score and displays it.
+  // If dice is not equal to 1 it adds the dice number to current score and displays it.
   if (dice !== 1) {
     currentScore += dice;
-    elementCurrentScore0.textContent = currentScore;
-  } else {
+    document.getElementById(`current--${currentPlayer}`).textContent =
+      currentScore;
+  }
+
+  // If dice is 1 it will reset the current score to 0 and switch the current player.
+  else {
+    document.getElementById(`current--${currentPlayer}`).textContent = 0;
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    elementPlayer0.classList.toggle('player--active'); // Changes the display of the element.
+    elementPlayer1.classList.toggle('player--active'); // Changes the display of the element.
   }
 });
